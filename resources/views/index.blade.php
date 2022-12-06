f34c3bfe9430b24cb2762abd69f7607a3c783e23
unknown <djkachempire@gmail.com>
2022-12-06 17:57:09 +0100

    add css link

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,11 +4,11 @@
 
       <div class="slider_brand" style="position:relative; z-index: -1;">
             <div class="flex w-100 mx-auto  items-center justify-center " 
-                style="background-image: url('img/librarie.jpg'); height: 90vh;  background-position: center;
+                style="background-image: url('img/librarie.png'); height: 90vh;  background-position: center;
                 background-size: cover;">
                 <div class="flex items-center justify-center flex-col " style="width: 80vw; margin: 0 auto;" >
                     <h1 class="text-white text-7xl">Telecharger des Miliers de livre</h1>
-                    <p class="text-gray-500 text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
+                    <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                          soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, accusantium iste sapiente.</p>
                     <div>
                         <button class="bg-yellow-500 text-white p-2 rounded shadow-md mt-4">Explorer Maintenant</button>
@@ -16,11 +16,11 @@
                 </div>
             </div>
             <div class="grid pb-10 w-100 mx-auto  items-center justify-items-center " 
-            style="background-image: url('img/events.jpg'); height: 90vh; background-position: center;
+            style="background-image: url('img/events.png'); height: 90vh; background-position: center;
             background-size: cover;">
                     <div class="flex items-center justify-center flex-col" style="width: 80vw; margin: 0 auto;" >
                         <h1 class="text-white text-7xl text-center">Participer à des évenements autour du livre</h1>
-                        <p class="text-gray-500 text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                        <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                             Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                             soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, 
                             accusantium iste sapiente.</p>
@@ -31,11 +31,11 @@
                 </div>
             
                 <div class="grid pb-10 w-100 mx-auto  items-center justify-items-center " 
-                style="background-image: url('img/association.jpg'); height: 90vh; background-position: center;
+                style="background-image: url('img/association.png'); height: 90vh; background-position: center;
                 background-size: cover;">
                         <div class="flex items-center justify-center flex-col" style="width: 80vw; margin: 0 auto;">
                             <h1 class="text-white text-7xl">Rejoindre notre association</h1>
-                            <p class="text-gray-500 text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                            <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                                 Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                                 soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, 
                                 accusantium iste sapiente.</p>
