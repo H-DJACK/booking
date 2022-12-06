b725edc04fc053c26c1c7807d56eb037be097246
unknown <djkachempire@gmail.com>
2022-12-06 19:41:54 +0100

    add new image

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,26 +6,29 @@
             <div class="flex w-100 mx-auto  items-center justify-center " 
                 style="background-image: url('img/librarie.jpg'); height: 90vh;  background-position: center;
                 background-size: cover;">
-                <div class="flex items-center justify-center flex-col " style="width: 80vw; margin: 0 auto;" >
-                    <h1 class="text-white text-7xl">Telecharger des Miliers de livre</h1>
+                <div class="flex items-center justify-center flex-col " style="width: 60vw; margin: 30vh auto;" >
+                    <h1 class="text-white text-7xl font-bold">Telecharger des Miliers de livre</h1>
                     <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
+                         soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, accusantium iste sapiente.Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                          soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, accusantium iste sapiente.</p>
                     <div>
-                        <button class="bg-yellow-500 text-white p-2 rounded shadow-md mt-4">Explorer Maintenant</button>
+                        <button class="bg-yellow-500 text-white text-xl p-2 rounded shadow-md mt-4">Explorer Maintenant</button>
                     </div>
                 </div>
             </div>
             <div class="grid pb-10 w-100 mx-auto  items-center justify-items-center " 
             style="background-image: url('img/events.png'); height: 90vh; background-position: center;
             background-size: cover;">
-                    <div class="flex items-center justify-center flex-col" style="width: 80vw; margin: 0 auto;" >
-                        <h1 class="text-white text-7xl text-center">Participer à des évenements autour du livre</h1>
-                        <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
+                    <div class="flex items-center justify-center flex-col" style="width: 60vw; margin: 30vh auto;" >
+                        <h1 class="text-white text-7xl text-center font-bold">Participer à des évenements autour du livre</h1>
+                        <p class="text-white text-lg ">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                             Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                             soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, 
-                            accusantium iste sapiente.</p>
+                            accusantium iste sapiente.
+                            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
+                         soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, accusantium iste sapiente.</p>
                         <div>
-                            <button class="bg-yellow-500 text-white p-2 rounded shadow-md mt-4">Participer Maintenant</button>
+                            <button class="bg-yellow-500 text-white text-xl p-2 rounded shadow-md mt-4">Participer Maintenant</button>
                         </div>
                     </div>
                 </div>
@@ -33,14 +36,16 @@
                 <div class="grid pb-10 w-100 mx-auto  items-center justify-items-center " 
                 style="background-image: url('img/association.jpg'); height: 90vh; background-position: center;
                 background-size: cover;">
-                        <div class="flex items-center justify-center flex-col" style="width: 80vw; margin: 0 auto;">
-                            <h1 class="text-white text-7xl">Rejoindre notre association</h1>
+                        <div class="flex items-center justify-center flex-col" style="width: 60vw; margin: 30vh auto;">
+                            <h1 class="text-white text-7xl font-bold">Rejoindre notre association</h1>
                             <p class="text-white text-lg">Lorem ipsum dolor, sit amet consectetur adipisicing elit. 
                                 Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
                                 soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, 
-                                accusantium iste sapiente.</p>
+                                accusantium iste sapiente.
+                                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nam iusto aliquam odit quidem perspiciatis, nemo harum culpa
+                         soluta praesentium reprehenderit minus! Aspernatur similique rerum quo sit vel, accusantium iste sapiente.</p>
                             <div>
-                                <button class="bg-yellow-500 text-white p-2 rounded shadow-md mt-4">Nous rejoindre</button>
+                                <button class="bg-yellow-500 text-white text-xl p-2 rounded shadow-md mt-4">Nous rejoindre</button>
                             </div>
                         </div>
                     </div>
@@ -49,7 +54,7 @@
 
         <div class="grid grid-cols-2 mx-10 py-10 mt-4 padding-about ">
             <div class="flex flex-col justify-center items-center ">
-                <h1 class="text-4xl text-center py-8 mx-auto text-[#000050] font-bold">Take care about your book</h1>
+                <h1 class="text-5xl text-center py-8 mx-auto text-[#000050] font-bold">Take care about your book</h1>
                 <p class="text-slate-500 text-justify text-lg w-90">
                     Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
                     Repellendus omnis amet blanditiis hic ratione corporis 
@@ -66,14 +71,14 @@
         </div>-
 
         <section class="bg-slate-50">
-            <h1 class="text-4xl text-center py-8 mx-auto text-[#000050] font-bold">Nos Caregories</h1>
+            <h1 class="text-5xl text-center py-8 mx-auto text-[#000050] font-bold">Nos Caregories</h1>
             <div class="grid grid-cols-6 gap-10 padding py-8 ">
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Recommended</button>
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Motivation</button>
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Art</button>
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Religion</button>
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Roman</button>
-                <button class="bg-yellow-500 hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Poesy</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Recommended</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Motivation</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Art</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Religion</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Roman</button>
+                <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Poesy</button>
             </div>
         
             <div class="flex bg-gray-200">
@@ -90,9 +95,9 @@
                 <div id="sliderContainer" class="w-10/12 overflow-hidden" >
                     <ul id="slider" class="flex w-full transition-margin duration-700 slider">
                         <li class="p-5">
-                            <div class="justify-center grid items-start shadown-lg w-full  bg-gray-100 rounded" >
+                            <div class="justify-center grid items-start shadown-lg  bg-gray-100 rounded" >
                                 <a href="#">
-                                <img src="img/1.jpg" class="mx-auto w-full h-60 rounded-t-lg " width="100" height="200" alt="kffl">
+                                <img src="img/1.jpg" class="mx-auto w-full h-60 rounded-t-lg " width="250" height="250" alt="kffl">
                                 <h4 class="font-bold text-center w-30 text-xl mt-2">Le meutre </h4>
                                 <div class="p-2">
                                     <p class="text-slate-500 text-sm">Paul Eluad</p>
@@ -182,56 +187,74 @@
                 </div>
             </div>
         <div class="mx-auto py-10 flex items-center justify-center">
-            <button class="bg-yellow-500  hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Voir plus</button>
+            <button class="bg-yellow-500 text-xl  hover:bg-yellow-600 text-white p-2 rounded shadow-md mt-4">Voir plus</button>
         </div>
     </section>
      
 
-    <section  class="bg-gray-800 py-8 padding mt-4">
-        <h1 class="text-4xl mx-auto py-8 text-gray-100 font-bold">Nouveau événement</h1>
+    <section  class=" py-8 padding mt-4" >
+        <h1 class="text-5xl text-center py-8 mx-auto text-[#000050] font-bold">Nouveau événement</h1>
         <div class="grid grid-cols-4 gap-8 ">
-            <div class="flex flex-col justify-center items-center bg-neutral-100 p-10 rounded">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Science</p>
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/events.png'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Festival du livre</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Roman</p>
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/events.png'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Dedicace speciale</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Art</p>
+
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/association.jpg'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Festival du livre</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Bibliography</p>
+
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/association.jpg'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Dedicace speciale</p>
+                <button class="bg-blue-500  hhover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Comics</p>
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/events.png'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Festival du livre</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Poesy</p>
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/events.png'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Dedicace speciale</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Ingenery</p>
+
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/association.jpg'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Festival du livre</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
-            <div class="flex flex-col justify-center items-center  bg-neutral-100 p-10 rounded shadow-md">
-                <img src="img/logo1.png" width="80" height="130" alt="logo" />
-                <p>Medecine</p>
+
+            <div class="flex flex-col justify-center items-center rounded-t-lg bg-white max-w-sm"  style="background-image: url('img/association.jpg'); height: 30vh;  background-position: center;
+                background-size: cover; position: relative; z-index: 1;">
+           
+                <p class="text-white text-4xl font-medium mb-2"  style="position: abolute; z-index: 2;">Dedicace speciale</p>
+                <button class="bg-blue-500  hover:bg-blue-700 text-white p-2 rounded shadow-md mt-4">Suivre l evenement</button>
             </div>
         </div>
         <div class="mx-auto py-10 flex items-center justify-center">
-            <button class="bg-yellow-500 text-white p-2 rounded shadow-md mt-4">Plus d'événement</button>
+            <button class="bg-yellow-500 hover:bg-yellow-600 text-xl text-white p-2 rounded shadow-md mt-4">Plus d'événement</button>
         </div>
     </section>
        
 
-    <section class="grid grid-cols-5 py-4 my-10 gap-6 ml-8 mr-8 py-8 padding">
-        <h2 class="text-4xl mx-auto py-8 text-[#000050] font-bold">A paraitre</h2>
-        <div>
+    <section class="py-4 my-10 ml-8 mr-8 py-8 padding">
+        <h2 class="text-5xl mx-auto py-8 text-[#000050] font-bold">A paraitre</h2>
+        <div class="grid grid-cols-7 gap-4">
             <div class="justify-center grid items-start shadown-lg  bg-gray-100 rounded" >
                 <a href="#">
                 <img src="img/1.jpg" class="mx-auto w-full h-60 rounded-t-lg " width="100" height="200" alt="kffl">
@@ -245,7 +268,7 @@
     
            <div class="justify-center grid  bg-gray-100 items-start" >
                 <a href="#">
-                <img src="img/2.jpg" class="mx-auto w-full h-60 rounded-t-lg" width="150" height="300" alt="kffl">
+                <img src="img/8.jpg" class="mx-auto w-full h-60 rounded-t-lg" width="150" height="300" alt="kffl">
                 <h4 class="font-bold text-center w-30 text-xl mt-2">Le meutre</h4>
                 <div class="p-2">
                     <p class="text-slate-500 text-sm">Paul Eluad</p>
@@ -257,7 +280,7 @@
     
            <div class="justify-center grid  bg-gray-100 items-start" >
                 <a href="#">
-                <img src="img/3.jpg" class="mx-auto w-full h-60 rounded-t-lg" width="150" height="300" alt="kffl">
+                <img src="img/9.jpg" class="mx-auto w-full h-60 rounded-t-lg" width="150" height="300" alt="kffl">
                 <h4 class="font-bold text-center w-30 text-xl mt-2">Le meutre</h4>
                 <div class="p-2">
                     <p class="text-slate-500 text-sm">Paul Eluad</p>
